45ef74b13d29af84fcb740e0e003080be0c2b71f
Pandoxo <wozniak.hubert.pb+github@gmail.com>
2025-11-04 11:48:25 +0100

    initial

---
+++ primepathsshort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <queue>
+#include <algorithm>
+#include <math.h>
+
+
+int* sieve(int& n, int ){
+
+
+}
+
+
+int main(){
+    return 0;
+}
